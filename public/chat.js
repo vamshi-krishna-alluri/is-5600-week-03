a07539853814490be30aa7bc9676a0e8c49fd2ac
vamshi-krishna-alluri <allurivamshi143@gmail.com>
2024-11-13 02:03:08 +0000

    Lab submission

--- public/chat.js
+++ public/chat.js
@@ -2,8 +2,6 @@ new window.EventSource("/sse").onmessage = function(event) {
     window.messages.innerHTML += `<p>${event.data}</p>`;
   };
 
-
-  
   window.form.addEventListener('submit', function(event) {
     event.preventDefault();
 
